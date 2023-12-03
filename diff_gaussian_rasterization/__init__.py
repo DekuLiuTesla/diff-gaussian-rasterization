d0782a2c766dcd1d55fafd5611ae534a337ae35d
Lue FAN <1060056270@qq.com>
2023-12-03 08:16:39 +0000

    add missing commit from the last one

--- diff_gaussian_rasterization/__init__.py
+++ diff_gaussian_rasterization/__init__.py
@@ -91,17 +91,17 @@ class _RasterizeGaussians(torch.autograd.Function):
         if raster_settings.debug:
             cpu_args = cpu_deep_copy_tuple(args) # Copy them before they can be corrupted
             try:
-                num_rendered, color, out_extra_feats, radii, geomBuffer, binningBuffer, imgBuffer, transmittance, num_occluder = _C.rasterize_gaussians(*args)
+                num_rendered, color, out_extra_feats, radii, geomBuffer, binningBuffer, imgBuffer, transmittance, num_covered_pixels = _C.rasterize_gaussians(*args)
             except Exception as ex:
                 torch.save(cpu_args, "snapshot_fw.dump")
                 print("\nAn error occured in forward. Please forward snapshot_fw.dump for debugging.")
                 raise ex
         else:
-            num_rendered, color, out_extra_feats, radii, geomBuffer, binningBuffer, imgBuffer, transmittance, num_occluder = _C.rasterize_gaussians(*args)
+            num_rendered, color, out_extra_feats, radii, geomBuffer, binningBuffer, imgBuffer, transmittance, num_covered_pixels = _C.rasterize_gaussians(*args)
 
         # Keep relevant tensors for backward
         if raster_settings.record_transmittance:
-            return transmittance, num_occluder, radii
+            return transmittance, num_covered_pixels, radii
 
         ctx.raster_settings = raster_settings
         ctx.num_rendered = num_rendered
